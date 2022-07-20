bbcc5e8e771fdebecbee781a5a40ec6fee737820
ulyana.metlikina <metlikina@skbkontur.ru>
2022-07-20 17:55:12 +0500

    add Metanit Strings

---
+++ MetanitStrings/MetanitStrings/Example.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MetanitStrings
+{
+    // определение класса Person
+    public class Person 
+    {
+        public Person()
+        {
+        }
+        
+        public Person(string name, int age)
+        {
+            _age = age;
+            _name = name;
+        }
+
+        private string _name = "Undefined";
+        private readonly int _age;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        
+        public int Age { get; set; }
+     
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {_name}  Возраст: {_age}");
+        }
+        
+        public void Print2()
+        {
+            Console.WriteLine($"Имя: {Name}  Возраст: {Age}");
+        }
+    }
+}
---
+++ MetanitStrings/MetanitStrings/StringsClass.cs
@@ -0,0 +1,155 @@
+﻿namespace MetanitStrings
+{
+    public class StringsClass
+    {
+         //         // var ex1 = "строка";
+                //         // var ex2 = new string('u', 10);
+                //         // var ex3 = new string(new[] {'m', 'o', 't', 'o'});
+                //         // var ex4 = new string(new[] {'m', 'o', 't', 'o'}, 0, 4);
+                //         //
+                //         // Console.WriteLine(ex1);
+                //         // Console.WriteLine(ex2);
+                //         // Console.WriteLine(ex3);
+                //         // Console.WriteLine(ex4);
+                //         //
+                //         // //получение по индексу любого символа в строке
+                //         // var variable = "Задачки";
+                //         // var takeAnyChar = variable[3];
+                //         //
+                //         // Console.WriteLine(takeAnyChar);
+                //         // Console.WriteLine(variable.IndexOf((char) 1));
+                //         // Console.WriteLine(variable.Length);
+                //         //
+                //         // //Compare
+                //         // var s1 = "Diadoc1";
+                //         // var s2 = "diadoc2";
+                //         //
+                //         // var resultCompare = string.Compare(s1, s2);
+                //         //
+                //         // Console.WriteLine(string.Compare(s1, s2, true));
+                //         //
+                //         // //CompareOrdinal
+                //         // var b1 = "Diadoc1";
+                //         // var b2 = "diadoc2";
+                //         // int resultB;
+                //         //
+                //         // Console.WriteLine("Надеюсь, этот метод не учитывает регистр");
+                //         // resultB = string.CompareOrdinal(b1, b2);
+                //         // Console.WriteLine(resultB);
+                //         //
+                //         // //Contains
+                //         // var c1 = "Diadoc";
+                //         // var c2 = "diadoc";
+                //         // var c3 = "doc";
+                //         //
+                //         // var result1 = s1.Contains(c3);
+                //         // var result2 = s1.Contains(c2);
+                //         //
+                //         // Console.WriteLine("{result1} against {result2}");
+                //         //
+                //         // //Concat
+                //         // var d1 = "Diadoc";
+                //         // var d2 = "diadoc";
+                //         //
+                //         // Console.WriteLine("Concatenated string is '{0}'", string.Concat(s1,s2));
+                //         //
+                //         // //Concat2
+                //         // var e1 = "Ульяна";
+                //         // var e2 = "Метликина";
+                //         // var e3 = "Геннадьевна";
+                //         //
+                //         // Console.WriteLine("My name is {0}.", string.Concat(string.Concat(e2, e1), e3));
+                //         
+                //         // CopyTo
+                //         // char [] destination = {'М','е','т','л','и','к','и','н','а',' ','У','л','ь','я','н','а'};
+                //         // string surName = "Горелова";
+                //         // surName.CopyTo(0, destination, 0, surName.Length);
+                //         // Console.WriteLine(destination);
+                //         
+                //         //EndaWith(Char) (с учетом регистра, без учета языка)
+                //        
+                //         //EndsWith(String)
+                //         // string aa = "Горелова";
+                //         // string bb = "пова";
+                //         // bool EndResult;
+                //         // Console.WriteLine(EndResult = aa.EndsWith("ова"));
+                //         // Console.WriteLine(EndResult = aa.EndsWith(bb));
+                //
+                //         //EndsWith (char) ???
+                //         
+                //         
+                //         //Format
+                //         // string name = "Kate";
+                //         // double cash = 20.25;
+                //         // string result = string.Format("{0}, You owe me {1:C2}. Today is {2}!", name, cash, DateTime.Now);
+                //         // Console.WriteLine(result);
+                //         
+                //         //IndexOF(Char)
+                //         // string s1 = "Строка1";
+                //         // int index = s1.IndexOf('о');
+                //         // Console.WriteLine(index);
+                //         
+                //         //IndexOf(String)
+                //         // string ss1 = "Здесь будем искать";
+                //         // string ss2 = "искать";
+                //         // Console.WriteLine(ss1.IndexOf(ss2));
+                //         //
+                //         //Insert(индекс вставки в исходной строке, вставляемая строка)
+                //         // string initial = "Метликина";
+                //         // string modified = (initial.Insert(9, " Ульяна"));
+                //         // var result = string.Concat(modified, " Геннадьевна");
+                //         // Console.WriteLine(result);
+                //         
+                //         //Join(String, String)
+                //         string q1 = "Метликина";
+                //         string q2 = "Ульяна";
+                //         string q3 = "Геннадьевна";
+                //         var values1 = new string[] {q1, q2, q3}; //альтернативный вариант, почему?
+                //         string[] values2 = new string[] {q1, q2, q3};
+                //         
+                //         string resultString = string.Join(" ", values2);
+                //         Console.WriteLine(resultString);
+                //         
+                //         // // Replace(старое значение, новое значение)
+                //         //  string name = "У л ьяна";
+                //         //  string replaced1 = name.Replace(" ", "");
+                //         //  // string result = replaced1.Replace("ья", "иа");
+                //         //  Console.WriteLine(replaced1);
+                //         
+                //         // //Split
+                //         //  string str1 = "Метликина Ульяна Геннадьевна";
+                //         //  string[] substrings = str1.Split(' ');
+                //         //  foreach (var i in substrings)
+                //         //  {
+                //         //      Console.WriteLine(i);
+                //          // }
+                //          
+                //              // Console.WriteLine(substrings);
+                //         
+                //         // //ToLower
+                //         // string aaa = "МЕТЛИКИНА Ульяна";
+                //         // string result = aaa.ToLower();
+                //         // Console.WriteLine(result);
+                //         //
+                //         // //ToUpper
+                //         // string bbb = "метликина ульяна";
+                //         // // string result2 = bbb.ToUpper();
+                //         // Console.WriteLine(bbb.ToUpper());
+                //         
+                //         //Trim
+                //         
+                //         // string www = " Ульяна"
+                //         
+                //         Person tom = new Person()
+                //         {
+                //             Name = "Dmitry",
+                //             Age = 15
+                //         };  // создание объекта класса Person
+                //
+                //         tom.Age = 25;
+                //         
+                //         Person john = new Person("John", 25);
+                //         john.Print();
+                //
+    }
+}
