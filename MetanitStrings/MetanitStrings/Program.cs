9607adb14689f6c3c96c4082313d10570f849899
ulyana.metlikina <metlikina@skbkontur.ru>
2022-07-21 16:27:42 +0500

    some changes

--- MetanitStrings/MetanitStrings/Program.cs
+++ MetanitStrings/MetanitStrings/Program.cs
@@ -1,40 +1,40 @@
 ﻿using System;
 using System.Linq;
 
-namespace MetanitStrings
-{
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            int a = 30;
-            int b = 31;
-            Console.WriteLine(a);
-            Print(11, b);
-        }
-
-        public static void Print(int var1, int b)
-        {
-            var a = var1;
-            Console.WriteLine(a);
-
-            // Вывести результат того, что поменялось (измененной переменной)
-        }
-        // private static void Change(int var1)
-        // {
-        //     a = var1;
-        //     // b = var2;
-        //     string result = $"Hello {a}";
-        //     Console.WriteLine(result);
-        //     
-        //     return result;
-        // }
-        // public static void PrintChanges()
-        // {
-        //     Console.WriteLine(Change(100));
-        }
-    }
-
+// namespace MetanitStrings
+// {
+//     public static class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             int a = 30;
+//             int b = 31;
+//             Console.WriteLine(a);
+//             Print(11, b);
+//         }
+//
+//         public static void Print(int var1, int b)
+//         {
+//             var a = var1;
+//             Console.WriteLine(a);
+//
+//            
+//         }
+    //     // private static void Change(int var1)
+    //     // {
+    //     //     a = var1;
+    //     //     // b = var2;
+    //     //     string result = $"Hello {a}";
+    //     //     Console.WriteLine(result);
+    //     //     
+    //     //     return result;
+    //     // }
+    //     // public static void PrintChanges()
+    //     // {
+    //     //     Console.WriteLine(Change(100));
+    //     }
+    // }
+    //
 
 
 
@@ -69,33 +69,40 @@ namespace MetanitStrings
 
 
 
-//     // 2. Создать класс с двумя переменными. Добавить конструктор с входными параметрами. Добавить конструктор, инициализирующий члены класса по умолчанию.
-//     
-//     class Program
-//     {
-//         public static void Main(string[] args)
-//         {
-//             Exercise2 ex2 = new Exercise2();
-//         }
-//     }
-//
-//     public class Exercise2
-//     {
-//         public string newstr1;
-//         public string newstr2;
-//         
-//         public Exercise2()
-//         {
-//             newstr1 = "Hello";
-//             newstr2 = "World";
-//         }
-//
-//         public Exercise2(string str1, string str2)
-//         {
-//             newstr1 = str1;
-//             newstr2 = str2;
-//         }
-//     }
+     // 2. Создать класс с двумя переменными. Добавить конструктор с входными параметрами. Добавить конструктор, инициализирующий члены класса по умолчанию.
+     
+     class Program
+     {
+         public static void Main(string[] args)
+         {
+             Exercise2 ex2 = new Exercise2();
+         }
+     }
+
+     public class Exercise2
+     {
+         public string newstr1;
+         public string newstr2;
+
+         public Exercise2()
+         {
+             newstr1 = "Hello";
+             newstr2 = "World";
+             Console.WriteLine(newstr1);
+             Console.WriteLine(newstr2);
+         }
+
+         public void Print(string str1, string str2)
+         {
+             var s = str2;
+             Console.WriteLine(s);
+         }
+         public Exercise2(string str1, string str2)
+         {
+             newstr1 = str1;
+             newstr2 = str2;
+         }
+     }
 //     
 //     
 // }
